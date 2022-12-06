2babef0750463486c13adeb937b9f0019e5a3e62
Timur Ulanovsky <ulanovskytimur@gmail.com>
2022-12-06 16:13:24 +0200

    Added CORS. Some Changes.

--- Data/Data/MockData.cs
+++ Data/Data/MockData.cs
@@ -22,7 +22,7 @@ namespace Data.Data
                     Price = 73999,
                     ColorId = 1,
                     CategoryId = 3,
-                    ImagePath = @"https://samsungshop.com.ua/res/cache/images/7d/15/7d153c06cf8aeafa0307b002847130ad/ad6844aa1e4caa49c317ac5114ec9346.jpg"
+                    ImagePath = @"https://www.pngall.com/wp-content/uploads/5/Samsung-TV-PNG-Image-HD.png"
                 },
                 new Product()
                 {
@@ -33,7 +33,7 @@ namespace Data.Data
                     Price = 62999,
                     ColorId = 1,
                     CategoryId = 3,
-                    ImagePath = @"https://samsungshop.com.ua/res/cache/images/1d/90/1d90f289e972b72228c32289202be042/e8d9a3ca400b66bf8c8c692f14da64a2.jpg"
+                    ImagePath = @"https://www.pngall.com/wp-content/uploads/5/Samsung-TV-PNG-Image-HD.png"
                 },
                 new Product()
                 {
@@ -44,7 +44,7 @@ namespace Data.Data
                     Price = 52199,
                     CategoryId = 3,
                     ColorId = 3,
-                    ImagePath = @"https://samsungshop.com.ua/res/cache/images/40/19/401937ab040b932e4f2c1e9337ddd3c0/15a98e0cd6a396d4bca1ca2dc6617e43.jpg"
+                    ImagePath = @"https://www.pngall.com/wp-content/uploads/5/Samsung-TV-PNG-Image-HD.png"
                 },
                 new Product()
                 {
@@ -55,7 +55,7 @@ namespace Data.Data
                     Price = 28399,
                     ColorId = 2,
                     CategoryId = 3,
-                    ImagePath = @"https://samsungshop.com.ua/res/cache/images/1c/4f/1c4f73fd470d63249f4a0763dff03d89/4262c548db10324900e61c6853ce44f8.jpg"
+                    ImagePath = @"https://www.pngall.com/wp-content/uploads/5/Samsung-TV-PNG-Image-HD.png"
                 }
             };
         }
---
+++ Data/Migrations/20221206132832_ImageChanges.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImageChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImagePath",
+                value: "https://www.pngall.com/wp-content/uploads/5/Samsung-TV-PNG-Image-HD.png");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImagePath",
+                value: "https://www.pngall.com/wp-content/uploads/5/Samsung-TV-PNG-Image-HD.png");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImagePath",
+                value: "https://www.pngall.com/wp-content/uploads/5/Samsung-TV-PNG-Image-HD.png");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ImagePath",
+                value: "https://www.pngall.com/wp-content/uploads/5/Samsung-TV-PNG-Image-HD.png");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImagePath",
+                value: "https://samsungshop.com.ua/res/cache/images/7d/15/7d153c06cf8aeafa0307b002847130ad/ad6844aa1e4caa49c317ac5114ec9346.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImagePath",
+                value: "https://samsungshop.com.ua/res/cache/images/1d/90/1d90f289e972b72228c32289202be042/e8d9a3ca400b66bf8c8c692f14da64a2.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImagePath",
+                value: "https://samsungshop.com.ua/res/cache/images/40/19/401937ab040b932e4f2c1e9337ddd3c0/15a98e0cd6a396d4bca1ca2dc6617e43.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ImagePath",
+                value: "https://samsungshop.com.ua/res/cache/images/1c/4f/1c4f73fd470d63249f4a0763dff03d89/4262c548db10324900e61c6853ce44f8.jpg");
+        }
+    }
+}
