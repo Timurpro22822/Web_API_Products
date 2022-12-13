c3d837a75a2a7cc17ccaca566f82c37adff343ac
Timur Ulanovsky <ulanovskytimur@gmail.com>
2022-12-13 09:04:43 +0200

    Added UserResponseDTO

---
+++ BusinessLogic/DTOs/UserResponseDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.DTOs
+{
+    public class UserResponseDTO
+    {
+        public bool IsSuccesfulRegistration { get; set; }
+        public IEnumerable<string>? Errors { get; set; }
+    }
+}
